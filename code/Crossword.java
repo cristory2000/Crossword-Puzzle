fd9803a9df586d990861f25e49366e273b9615be
Cristopher  Story <cristory2000@gmail.com>
2021-09-29 20:00:38 -0400

    with score now

--- code/Crossword.java
+++ code/Crossword.java
@@ -10,41 +10,37 @@ public class Crossword
     static StringBuilder colStr[];
     static StringBuilder rowStr[];
     static int BoardSize;
-
+    static int score;
+    static Map<Character,Character> letters= new HashMap<>();
     public static void main(String args[]) throws IOException
     {
             //input the dictionary file
             
             Scanner fileScan = new Scanner(new FileInputStream(args[0]));
             Scanner boardScan = new Scanner(new FileInputStream(args[1]));
-        
-                D=new MyDictionary();
+            Scanner scoreBoard = new Scanner(new FileInputStream("letterpoints.txt"));
+            D=new MyDictionary();
             //add dictionary file to whichever structure was chosen
             while(fileScan.hasNext())
             {
-                D.add(fileScan.next()) ;
-                
+                D.add(fileScan.next());
             }
             fileScan.close();
             crosswordBoardcreator(boardScan);
-            
+            //making the score hash
+            while(scoreBoard.hasNext())
+            {
+                String line=scoreBoard.nextLine();
+                letters.put(Character.toLowerCase(line.charAt(0)), line.charAt(2));
+            }
             colStr= new StringBuilder[BoardSize];
             rowStr= new StringBuilder[BoardSize];
             for (int i = 0; i < colStr.length; i++) {
                 colStr[i] = new StringBuilder("");
                 rowStr[i] = new StringBuilder("");
             }
-
-                for(int i=0;i<BoardSize;i++)
-                {
-                    for(int j=0;j<BoardSize;j++)
-                    {
-                       
-                        break;
-                    }
-                }
-                
                 crossFiller(0,0, rowStr, colStr);
+                
                 
     }
       
@@ -209,7 +205,22 @@ public class Crossword
             {
                 System.out.println(rowStr[i].toString());
             }
-            return;
+            for(int i=0;i<rowStr.length;i++)
+            {
+                 String word=rowStr[i].toString();
+                for(int j=0;j<word.length();j++)
+                {
+                 if(letters.containsKey(word.charAt(j)))
+                 {
+                     char v=letters.get(word.charAt(j));
+                     score+=Character.getNumericValue(v);
+                 }
+             }
+                
+         
+             }
+             System.out.println("The Score is: "+score);
+           
         }
         private static Coordinates nextCoordinates(int row, int col)
         {
@@ -229,12 +240,8 @@ public class Crossword
             }
             return result;
         }
-        public static int scoreChecker()
-        {
-            
-            return 1;
-        }
-        
+
+         
         //inner class
 	private static class Coordinates 
     {
